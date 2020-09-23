5f9a88a69c5b38cf34df59db7b687c0565498056
Hakjun Kim <hukanj@gmail.com>
2020-09-23 10:14:02 +0900

    Upload form tuned.

--- gaenyang/settings.py
+++ gaenyang/settings.py
@@ -82,12 +82,17 @@ WSGI_APPLICATION = 'gaenyang.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'djangogirls',
+        'NAME': 'gaenyang',
         'USER': 'name',
         'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '',
-    }
+    },
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # },
 }
 
 db_from_env = dj_database_url.config(conn_max_age=500)
