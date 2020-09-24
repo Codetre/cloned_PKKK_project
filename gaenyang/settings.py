0f9b64ae4de04b321489776e06ab18282478a37a
Hakjun Kim <hukanj@gmail.com>
2020-09-24 20:10:20 +0900

    Result layout align changed.

--- gaenyang/settings.py
+++ gaenyang/settings.py
@@ -80,19 +80,19 @@ WSGI_APPLICATION = 'gaenyang.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'gaenyang',
-        'USER': 'admin',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
-    },
-
     # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'gaenyang',
+    #     'USER': 'admin',
+    #     'PASSWORD': '',
+    #     'HOST': 'localhost',
+    #     'PORT': '',
     # },
+
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
 }
 
 db_from_env = dj_database_url.config(conn_max_age=500)
